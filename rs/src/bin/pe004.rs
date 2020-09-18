8212745a677692e8ebc25416dbd33ec5edf6abb6
Justin Browne <jebdude89@gmail.com>
2020-09-18 00:33:12 -0400

    Fix string stuff in pe004

--- rs/src/bin/pe004.rs
+++ rs/src/bin/pe004.rs
@@ -1,4 +1,3 @@
-/*
 use std::collections::HashSet;
 use std::io::Write;
 
@@ -7,26 +6,23 @@ fn is_palindrome(n: u32) -> bool {
     let mut v = Vec::<u8>::new();
     let _ = write!(&mut v, "{}", n);
     let s = String::from_utf8(v).unwrap();
-    for i in 0..(s.len() / 2) {
-        if s[i] == s[s.len() - i - 1] {
+    for (forward, backward) in s.chars().zip(s.chars().rev()) {
+        if forward != backward {
             res = false;
             break;
         }
     }
     res
 }
-*/
 
 fn main() {
-    /*
-        let mut s = HashSet::<u32>::new();
-        for i in 10..100 {
-            for j in i..100 {
-                if is_palindrome(i * j) {
-                    s.insert(i * j);
-                }
+    let mut s = HashSet::<u32>::new();
+    for i in 100..1000 {
+        for j in i..1000 {
+            if is_palindrome(i * j) {
+                s.insert(i * j);
             }
         }
-        println!("{}", s.iter().max().unwrap());
-    */
+    }
+    println!("{}", s.iter().max().unwrap());
 }
