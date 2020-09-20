224f0893365f8f7ec83c040b889098d2517b2684
Justin Browne <jebdude89@gmail.com>
2020-09-20 01:46:26 -0400

    Make pe087 simpler

--- rs/src/bin/pe087.rs
+++ rs/src/bin/pe087.rs
@@ -8,13 +8,22 @@ fn main() {
     let primes = get_primes_less_than_x(nth_root(MAX, 2) + 1);
 
     let mut nums = HashSet::new();
-    for i in primes.iter().take_while(|x| **x <= nth_root(MAX, 4)) {
-        let rem = MAX - u32::pow(*i, 4);
-        for j in primes.iter().take_while(|x| **x <= nth_root(rem, 3)) {
-            let rem = rem - u32::pow(*j, 3);
-            for k in primes.iter().take_while(|x| **x <= nth_root(rem, 2)) {
-                let rem = rem - u32::pow(*k, 2);
-                nums.insert(rem);
+    for i in primes
+        .iter()
+        .map(|x| x * x * x * x)
+        .take_while(|x| *x <= MAX)
+    {
+        for j in primes
+            .iter()
+            .map(|x| x * x * x)
+            .take_while(|x| *x <= MAX - i)
+        {
+            for k in primes
+                .iter()
+                .map(|x| x * x)
+                .take_while(|x| *x <= MAX - i - j)
+            {
+                nums.insert(i + j + k);
             }
         }
     }
